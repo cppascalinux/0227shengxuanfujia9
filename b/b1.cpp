ae217a24e3db0e47065134b67ea03e60bafa5425
cppascalinux <cppascalinux@gmail.com>
2019-03-01 22:39:36 +0800

    qwq

--- b/b1.cpp
+++ b/b1.cpp
@@ -5,7 +5,39 @@
 #define ll long long
 using namespace std;
 ll n;
-int a,b; 
+int a,b,tpa,tpb;
+ll ma[100],mb[100];
+int op[100],ans[100];
+void init()
+{
+	ma[0]=mb[0]=1;
+	for(tpa=1;(ma[tpa]=ma[tpa-1]*a)<=n;tpa++);
+	for(tpb=1;(mb[tpb]=mb[tpb-1]*b)<=n;tpb++);
+}
+ll find(ll x,int b,int *m,int s)
+{
+	ll t=x+1;
+	for(int i=0;i<=60;i++)
+	{
+		op[i]=t%b;
+		t/=b;
+	}
+	int bg=0;
+	for(int i=60;i>=0;i--)
+	{
+		if(bg)
+		{
+			ans[i]=min(b-1,max(0,s-(b-1)*i));
+			s-=ans[i];
+		}
+		else
+		{
+			
+		}
+	}
+	if(s)
+		return -1;
+}
 int main()
 {
 #ifndef ONLINE_JUDGE
@@ -17,7 +49,7 @@ int main()
 	while(t--)
 	{
 		scanf("%lld%d%d",&n,&a,&b);
-		
+		init();
 	}
 	return 0;
 }
